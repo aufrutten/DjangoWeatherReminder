e4edfce22632db853643627db1ebb598ef6fbc25
Semykopenko Ihor <aufrutten@gmail.com>
2023-04-10 14:18:45 +0200

    add JWT to REST API

--- root/main/urls.py
+++ root/main/urls.py
@@ -16,12 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt import views as jwt_views
+
 
 # <-----ADMIN----->
 urlpatterns = [path('admin/', admin.site.urls)]
 
+
 # <-----WeatherReminder----->
 urlpatterns += [path('', include('WeatherReminder.urls'))]
 
+
 # <-----API----->
 urlpatterns += [path('api-auth/', include('rest_framework.urls'))]
+urlpatterns += [path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair')]
+urlpatterns += [path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh')]
