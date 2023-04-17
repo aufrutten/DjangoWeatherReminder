5e9ad5e345453c1e101541ff99cf17c938cbfff5
Semykopenko Ihor <aufrutten@gmail.com>
2023-04-17 21:40:56 +0200

    add webpack with jquery and bootstrap5

---
+++ webpack/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, '../root', 'base_template', 'static', 'base', 'js'),
+        filename: 'bundle.js',
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource'
+            }
+        ]
+    }
+};
